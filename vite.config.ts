cc23f88d48646b56e06ec940a3cfe601a6be137d
bentyluxyry <bentyluxyry@gmail.com>
2025-12-20 05:45:45 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,10 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/app-lich/',  // ← Thêm dòng này, đúng tên repo
+})
